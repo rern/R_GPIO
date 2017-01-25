e23ec416482d2fa70f1ffeae0cdfad25e6c8b56b
rern <rernrern@gmail.com>
2017-01-25 19:36:45 +0700

    Update gpiouninstall.sh

--- gpiouninstall.sh
+++ gpiouninstall.sh
@@ -125,6 +125,13 @@ if [ $arg -eq 0 ]; then # skip if reinstall - gpiouninstall.sh <arg>
 	curl '127.0.0.1/clear'
 	echo
 
+	if pgrep midori > /dev/null; then
+		killall midori
+		sleep 1
+		startx  > /dev/null 2>&1 &
+		echo -e '\nLocal browser restarted.\n'
+	fi
+	
 	title2 "$runegpio successfully uninstalled."
 	titleend "$info Refresh browser for default $runeenh."
 fi
