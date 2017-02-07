4df665b73e174b3c94a358c243a71f3b97f9d82d
rern <rernrern@gmail.com>
2017-02-07 10:46:56 +0700

    Update gpioinstall.sh

--- gpioinstall.sh
+++ gpioinstall.sh
@@ -100,10 +100,12 @@ if ! pacman -Q python2-pip > /dev/null 2>&1 && ! pacman -Q python-pip > /dev/nul
 	pacman -S --noconfirm python2-pip
 	ln -s /usr/bin/pip2 /usr/bin/pip
 else
-	title "Get Pip packages file ..."
-	wget -q --show-progress -O varpip.tar "https://github.com/rern/RuneUI_GPIO/blob/master/_repo/varpip.tar?raw=1"
-	tar -xvf varpip.tar -C /
-	rm varpip.tar
+	if ! python -c "import mpd" > /dev/null 2>&1 || ! python -c "import requests" > /dev/null 2>&1; then
+		title "Get Pip packages file ..."
+		wget -q --show-progress -O varpip.tar "https://github.com/rern/RuneUI_GPIO/blob/master/_repo/varpip.tar?raw=1"
+		tar -xvf varpip.tar -C /
+		rm varpip.tar
+	fi
 fi
 
 if ! python -c "import mpd" > /dev/null 2>&1; then
