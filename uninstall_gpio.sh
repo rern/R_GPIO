cb392ddc84b7d17de20287192e09c2f9f58ecc7f
rern <rernrern@gmail.com>
2017-07-05 21:16:17 +0700

    Update uninstall_gpio.sh

--- uninstall_gpio.sh
+++ uninstall_gpio.sh
@@ -18,9 +18,7 @@
 # remove uninstall_gpio.sh
 
 # import heading function
-wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh
-. f_heading.sh
-rm f_heading.sh
+wget -qN https://github.com/rern/tips/raw/master/bash/f_heading.sh; . f_heading.sh; rm f_heading.sh
 
 runegpio=$( texrcolor "RuneUI GPIO" 6 )
 
