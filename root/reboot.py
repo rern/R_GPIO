fdea1762cbb02631813c735781561f3ed7bf859e
rern <rernrern@gmail.com>
2017-07-30 14:47:25 +0700

    Update reboot.py

--- root/reboot.py
+++ root/reboot.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+import sys
 import os
 
+if len(sys.argv) > 1:
+	part=sys.argv[1]
+	os.system('/usr/bin/sudo /bin/echo' part '> /sys/module/bcm2709/parameters/reboot_part')
 os.system('/var/www/command/rune_shutdown; reboot')
