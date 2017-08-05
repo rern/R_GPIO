ec76bcd82bc2685f10b33bbc27c00fa7b2809ad8
rern <rernrern@gmail.com>
2017-08-05 10:40:06 +0700

    Update gpioon.py

--- root/gpioon.py
+++ root/gpioon.py
@@ -41,27 +41,25 @@ data = {'conf': conf, 'pullup': pullup}
 
 print(json.dumps(data))
 
-if pullup == 0:
-	exit()
+if pullup == 1:
+	# broadcast pushstream (message non-char in curl must be escaped)
+	requests.post("http://localhost/pub?id=gpio", json="ON")
 
-# broadcast pushstream (message non-char in curl must be escaped)
-requests.post("http://localhost/pub?id=gpio", json="ON")
+	if on1 != 0:
+		GPIO.output(on1, 0)
+	if on2 != 0:
+		time.sleep(ond1)
+		GPIO.output(on2, 0)
+	if on3 != 0:
+		time.sleep(ond2)
+		GPIO.output(on3, 0)
+	if on4 != 0:
+		time.sleep(ond3)
+		GPIO.output(on4, 0)
 
-if on1 != 0:
-	GPIO.output(on1, 0)
-if on2 != 0:
-	time.sleep(ond1)
-	GPIO.output(on2, 0)
-if on3 != 0:
-	time.sleep(ond2)
-	GPIO.output(on3, 0)
-if on4 != 0:
-	time.sleep(ond3)
-	GPIO.output(on4, 0)
-	
-if GPIO.input(onx[1]) != 0:
-	requests.post("http://localhost/pub?id=gpio", json="FAILED")
-	exit()
+	if GPIO.input(onx[1]) != 0:
+		requests.post("http://localhost/pub?id=gpio", json="FAILED")
+		exit()
 
-if gpio['timer']['timer'] != 0:
-	os.system('/usr/bin/sudo /root/gpiotimer.py > /dev/null 2>&1 &')
+	if gpio['timer']['timer'] != 0:
+		os.system('/usr/bin/sudo /root/gpiotimer.py > /dev/null 2>&1 &')
