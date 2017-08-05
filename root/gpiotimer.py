7974149beec40a4c9d63f05e46697bdec75ac7e6
rern <rernrern@gmail.com>
2017-08-05 10:40:44 +0700

    Update gpiotimer.py

--- root/gpiotimer.py
+++ root/gpiotimer.py
@@ -10,22 +10,20 @@ with open('/srv/http/gpio.json') as jsonfile:
 
 timer = int(gpio['timer']['timer'])
 
-if timer == 0:
-	exit()
-
-i = timer
-while i >= 0:
-	time.sleep(60)
-	client = mpd.MPDClient(use_unicode=True)
-	client.connect("localhost", 6600)
-	state = client.status()['state']
-	client.close()
-	client.disconnect()
-	if state == "play":
-		i = timer
-	else:
-		i -= 1
-		if i == 1: # broadcast last loop
-			requests.post("http://localhost/pub?id=gpio", json=str(60) +"IDLE")
-		if i == 0:
-			os.system("/usr/bin/php /srv/http/gpiooff.php")
+if timer == 1:
+	i = timer
+	while i >= 0:
+		time.sleep(60)
+		client = mpd.MPDClient(use_unicode=True)
+		client.connect("localhost", 6600)
+		state = client.status()['state']
+		client.close()
+		client.disconnect()
+		if state == "play":
+			i = timer
+		else:
+			i -= 1
+			if i == 1: # broadcast last loop
+				requests.post("http://localhost/pub?id=gpio", json=str(60) +"IDLE")
+			if i == 0:
+				os.system("/usr/bin/php /srv/http/gpiooff.php")
