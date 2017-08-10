2e49d670fef7d65a93fe01d872f8dec38a4e0610
rern <rernrern@gmail.com>
2017-08-10 18:45:08 +0700

    Update uninstall_gpio.sh

--- uninstall_gpio.sh
+++ uninstall_gpio.sh
@@ -18,7 +18,7 @@
 # remove uninstall_gpio.sh
 
 # import heading function
-wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
+wget -qN https://raw.githubusercontent.com/rern/title_script/master/title.sh; . title.sh; rm title.sh
 runegpio=$( tcolor "RuneUI GPIO" )
 
 # check installed #######################################
