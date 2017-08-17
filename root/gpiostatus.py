deeda94e86a4485e3f8d09395ca1dd28b4a37347
rern <rernrern@gmail.com>
2017-08-17 19:20:14 +0700

    Update gpiostatus.py

--- root/gpiostatus.py
+++ root/gpiostatus.py
@@ -1,24 +1,5 @@
 #!/usr/bin/python
-import RPi.GPIO as GPIO
-import json
-
-with open('/srv/http/gpio.json') as jsonfile:
-	gpio = json.load(jsonfile)
-	
-on = gpio['on']
-off = gpio['off']
-
-on1 = int(on['on1'])
-on2 = int(on['on2'])
-on3 = int(on['on3'])
-on4 = int(on['on4'])
-
-onx = [on1, on2, on3, on4]
-onx = [i for i in onx if i != 0]
-
-GPIO.setwarnings(0)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(onx, GPIO.OUT)
+from gpio import *
 
 data = {'enable': gpio['enable']['enable'], \
 	'pullup': GPIO.input(onx[1])}
