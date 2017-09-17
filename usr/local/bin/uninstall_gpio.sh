dfd8ee2b7658a617960fbece15f2c76b943deccd
rern <rernrern@gmail.com>
2017-09-17 14:59:12 +0700

    Update uninstall_gpio.sh

--- usr/local/bin/uninstall_gpio.sh
+++ usr/local/bin/uninstall_gpio.sh
@@ -2,8 +2,7 @@
 
 alias=gpio
 
-# import heading function
-wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/title.sh; . title.sh; rm title.sh
+. /srv/http/title.sh
 
 # gpio off #######################################
 ./gpiooff.py &>/dev/null &
