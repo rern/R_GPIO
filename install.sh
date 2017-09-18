f4833cd5d7e9fc1a3c4f4cf2ffddb4b8e1f53a2f
rern <rernrern@gmail.com>
2017-09-18 21:42:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -132,9 +132,7 @@ sed -i -e 's/id="syscmd-poweroff"/id="poweroff"/
 ' $footer
 # no RuneUI enhancement
 ! grep -q 'pnotify3.custom.min.js' $footer &&
-sed -i $'$ a\
-<script src="<?=$this->asset(\'/js/vendor/pnotify3.custom.min.js\')?>"></script>
-' $footer
+echo '<script src="<?=$this->asset('"'"'/js/vendor/pnotify3.custom.min.js'"'"')?>"></script>' >> $footer
 
 [[ ! -f /etc/mpd.conf.gpio ]] && cp -rfv /etc/mpd.conf{,.gpio}
 
