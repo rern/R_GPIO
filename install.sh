33ba55c34beab4c79e59b3e0ba5214c357b11501
rern <rernrern@gmail.com>
2017-10-02 09:22:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ wgetnc https://github.com/rern/RuneUI_GPIO/archive/master.zip
 echo -e "$bar Install new files ..."
 rm -rf /tmp/install
 mkdir -p /tmp/install
-bsdtar -xvf master.zip --strip 1 -C /tmp/install
+bsdtar --exclude='.*' --exclude='*.md' -xvf master.zip --strip 1 -C /tmp/install
 rm master.zip /tmp/install/* &> /dev/null
 [[ -e /srv/http/gpio.json ]] && rm /tmp/install/srv/http/gpio.json
 
