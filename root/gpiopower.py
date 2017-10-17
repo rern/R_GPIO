2caf991c81632ed1e51d0a555085013862842ea1
rern <rernrern@gmail.com>
2017-10-17 10:32:21 +0700

    Add files via upload

---
+++ root/gpiopower.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import gpiooff
+import sys
+import os
+
+if len(sys.argv) == 1:
+	os.system('/var/www/command/rune_shutdown')
+	exit()
+	
+if sys.argv[1] != 'reboot':
+	cmd = '/usr/bin/sudo /bin/echo '+ sys.argv[1] +' > /sys/module/bcm2709/parameters/reboot_part'
+	os.system(cmd)
+	
+os.system('/var/www/command/rune_shutdown; reboot')
