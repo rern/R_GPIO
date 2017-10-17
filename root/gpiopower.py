8a54f7524a727a4903f6c7e1cff44ba0a3ee7a02
rern <rernrern@gmail.com>
2017-10-17 20:12:55 +0700

    Update gpiopower.py

--- root/gpiopower.py
+++ root/gpiopower.py
@@ -8,7 +8,6 @@ if len( sys.argv ) == 1:
 	exit()
 	
 if sys.argv[ 1 ] != 'reboot':
-	cmd = '/usr/bin/sudo /bin/echo '+ sys.argv[ 1 ] +' > /sys/module/bcm2709/parameters/reboot_part'
-	os.system( cmd )
+	os.system( '/usr/bin/sudo /bin/echo '+ sys.argv[ 1 ] +' > /sys/module/bcm2709/parameters/reboot_part' )
 	
 os.system( '/var/www/command/rune_shutdown; reboot' )
