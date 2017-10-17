ada3e379ad36a7f9369daa587c9070d605c955ba
rern <rernrern@gmail.com>
2017-10-17 10:32:03 +0700

    Delete reboot.py

--- root/reboot.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import gpiooff
-import sys
-import os
-
-if len(sys.argv) > 1:
-	cmd = '/usr/bin/sudo /bin/echo '+ sys.argv[1] +' > /sys/module/bcm2709/parameters/reboot_part'
-	os.system(cmd)
-os.system('/var/www/command/rune_shutdown; reboot')
