d5000aa2686eda4e8eaae485990745706c390ab0
rern <rernrern@gmail.com>
2018-01-03 20:46:44 +0700

    Update gpiotimerreset.php

--- srv/http/gpiotimerreset.php
+++ srv/http/gpiotimerreset.php
@@ -1,3 +1,3 @@
 <?php
 $pid = exec( '/usr/bin/sudo /usr/bin/pgrep gpiotimer.py' );
-if ( $pid ) exec( '/usr/bin/sudo /usr/bin/killall -9 gpiotimer.py; /root/gpiotimer.py &> /dev/null &' );
+if ( $pid ) exec( '/usr/bin/sudo /usr/bin/killall -9 gpiotimer.py; /root/gpiotimer.py' );
