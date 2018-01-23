e2d61660040349c52b5a714dfe80414dbf4c82a9
rern <rernrern@gmail.com>
2018-01-23 12:12:56 +0700

    Update gpiosettings.php

--- srv/http/gpiosettings.php
+++ srv/http/gpiosettings.php
@@ -56,7 +56,7 @@ $off4  = $off[ 'off4' ];
 $timer = $gpio[ 'timer' ][ 'timer' ];
 
 function optpin( $n ) {
-	$pinarray = array( 3,5,7,8,10,11,12,13,15,16,18,19,21,22,23,24,26,29,31,32,33,35,36,37,38,40 );
+	$pinarray = array( 11,13,15,16,18,19,21,22,23,24,26,29,31,32,33,36,37 );
 	$option = '';
 	foreach ( $pinarray as $pin ) {
 		$selected = ( $pin == $n ) ? ' selected' : '';
