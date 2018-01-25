1cf9bfaf2eb3ab7f9f7ec0c33121a677d50853f4
rern <rernrern@gmail.com>
2018-01-25 17:54:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ $offd = $off[\'offd1\'] + $off[\'offd2\'] + $off[\'offd3\'];\
 <input id="ond" type="hidden" value=<?=$ond ?>>\
 <input id="offd" type="hidden" value=<?=$offd ?>>
 ' -e '/Credits/ a\
-            <li style="cursor: pointer;"><a href="gpiosettings"><i class="fa fa-volume-off" style="width: 18px; font-size: 20px;"></i> GPIO</a></li>
+            <li style="cursor: pointer;"><a href="/gpiosettings.php"><i class="fa fa-volume-off" style="width: 18px; font-size: 20px;"></i> GPIO</a></li>
 ' -e '/class="home"/ a\
     <button id="gpio" class="btn btn-default btn-cmd"><i class="fa fa-volume-off fa-lg"></i></button>
 ' $file
