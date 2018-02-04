3b79c9ecd2e1018c9b64c70189105c3ee696ccbb
rern <rernrern@gmail.com>
2018-02-04 12:42:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ sed -i -e '/This switches output/{n;n;n;n; i\
                 <label class="col-sm-2 control-label" for="audio-output-interface">RuneUI GPIO</label>\
                 <div class="col-sm-10">\
                     <a class="btn btn-primary btn-lg" id="udacsave">Save</a>\
-                    <span class="help-block">Configure the rest of this page and save for <strong>RuneUI GPIO</strong> reloading.</span>\
+                    <span class="help-block">Configure the rest of this page and save for <strong>RuneUI GPIO</strong> reloading when power on.</span>\
                 </div>\
             </div> <?php /* gpio1 */?>
 }
