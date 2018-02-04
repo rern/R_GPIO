4998d10fcf0e6b3fa8cd8850504eb6093e7bb1bc
rern <rernrern@gmail.com>
2018-02-04 12:58:51 +0700

    Update gpiosave.php

--- srv/http/gpiosave.php
+++ srv/http/gpiosave.php
@@ -1,18 +1,4 @@
 <?php
-$redis = new Redis(); 
-$redis->pconnect( '127.0.0.1' );
-
-$aogpio = $redis->get( 'ao' );
-$volume = $redis->get( 'volume' );
-$acards = $redis->hGetAll( 'acards' );
-$mpdconf = $redis->hGetAll( 'mpdconf' );
-
-$redis->set( 'enablegpio', $_POST[ 'enable' ] );
-$redis->set( 'aogpio', $aogpio );
-$redis->set( 'volumegpio', $volume );
-$redis->hMset( 'acardsgpio', $acards );
-$redis->hMset( 'mpdconfgpio', $mpdconf );
-
 $gpio = array(
 	  'pin'    => array(
 		  'pin1'   => $_POST[ 'pin1' ]
