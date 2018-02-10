526c42189c6eab91a23b41f3a7672640745ca375
rern <rernrern@gmail.com>
2018-02-10 11:37:19 +0700

    Add files via upload

--- srv/http/gpioexec.php
+++ srv/http/gpioexec.php
@@ -4,51 +4,52 @@ $onoffpy = $_GET[ 'onoffpy' ];
 if ( $onoffpy === 'gpiotimer.py' ) {
 	exec( '/usr/bin/sudo /usr/bin/killall -9 /root/gpiotimer.py &> /dev/null' );
 	exec( '/usr/bin/sudo /root/gpiotimer.py &> /dev/null &' );
-} else if ( $onoffpy === 'gpiopower.py' ) {
-	$reboot = isset( $_GET[ 'reboot' ] ) ? ' reboot' : '';
-	exec( '/usr/bin/sudo /root/gpiopower.py'.$reboot.' &' );
-} else if ( $onoffpy === 'gpioon.py' ) {
-	// set mpd.conf
-	$redis = new Redis(); 
-	$redis->pconnect( '127.0.0.1' );
-
-	$ao = $redis->get( 'ao' );
-	$volume = $redis->get( 'volume' );
-	$mpdconf = $redis->hGetAll( 'mpdconf' );
-	
-	$aogpio = $redis->get( 'aogpio' );
-	$volumegpio = $redis->get( 'volumegpio' );
-	$mpdconfgpio = $redis->hGetAll( 'mpdconfgpio' );
-	
-	if ( $ao !== $aogpio || $volume !== $volumegpio || $mpdconf !== $mpdconfgpio ) {
-		$acardsgpio = $redis->hGetAll( 'acardsgpio' );
-		
-		$redis->set( 'ao', $aogpio );
-		$redis->set( 'volume', $volumegpio );
-		$redis->hMset( 'acards', $acardsgpio );
-		$redis->hMset( 'mpdconf', $mpdconfgpio );
-		
-		include( '/srv/http/app/libs/runeaudio.php' );
-		
-		wrk_mpdconf( $redis, 'switchao', $aogpio );
-		sleep( 2 );
-		wrk_mpdconf( $redis, 'restart' );
-	}
-} else {
-	$redis = new Redis(); 
-	$redis->pconnect( '127.0.0.1' );
-
-	$ao = $redis->get( 'ao' );
-	
-	if ( $ao !== 'bcm2835 ALSA_1' || $ao !== 'bcm2835 ALSA_2' ) {
-		include( '/srv/http/app/libs/runeaudio.php' );
-		
-		wrk_mpdconf( $redis, 'switchao', 'bcm2835 ALSA_1' );
-		sleep( 2 );
-		wrk_mpdconf( $redis, 'restart' );
-	}
+	die();
 }
 
-$state = exec( '/usr/bin/sudo /root/'.$onoffpy );
-// response to only initiator (no broadcast)
-echo $state;
+
+if ( $onoffpy === 'gpioon.py' ) {
+	$redis = new Redis(); 
+	$redis->pconnect( '127.0.0.1' );
+	$redis->set( 'asound0', shell_exec( '/usr/bin/cat /proc/asound/cards' ) ); // save asound before power on
+}
+
+echo exec( '/usr/bin/sudo /root/'.$onoffpy );
+
+if ( $onoffpy === 'gpioon.py' ) {
+	// set mpd.conf
+	$mpdconf = $redis->hGetAll( 'mpdconf' );
+	$mpdconfgpio = $redis->hGetAll( 'mpdconfgpio' );
+	
+	if ( $mpdconf === $mpdconfgpio ) die();
+	
+	$redis->set( 'ao0', $redis->get( 'ao' ) );
+	$aogpio = $redis->get( 'aogpio' );
+	$volumegpio = $redis->get( 'volumegpio' );
+	$redis->set( 'ao', $aogpio );
+	$redis->set( 'volume', $volumegpio );
+	foreach ( $mpdconfgpio as $key => $value ) {
+		$redis->hSet( 'mpdconf', $key, $value );
+	}
+	
+	include( '/srv/http/app/libs/runeaudio.php' );
+	
+	wrk_audioOutput($redis, 'refresh');         // refresh acards list: cat /proc/asound/cards
+	wrk_mpdconf( $redis, 'switchao', $aogpio ); // select acard + writecfg
+	wrk_mpdconf( $redis, 'restart' );           // restart mpd
+} else if ( $onoffpy === 'gpiooff.py' ) {
+	$redis = new Redis(); 
+	$redis->pconnect( '127.0.0.1' );
+	$asound0 = $redis->get( 'asound0' );
+	$redis->del( 'asound0' );
+	$asound1 = shell_exec( '/usr/bin/cat /proc/asound/cards' );
+	
+	if ( $asound0 === $asound1 ) die();
+
+	$ao0 = $redis->get( 'ao0' );
+	$redis->del( 'ao0' );
+
+	wrk_audioOutput($redis, 'refresh');
+	wrk_mpdconf( $redis, 'switchao', $ao0 );
+	wrk_mpdconf( $redis, 'restart' );
+}
