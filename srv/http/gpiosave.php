15b9adb11ac50bcd5dbe509e8c32df51bd802513
rern <rernrern@gmail.com>
2018-02-12 17:26:54 +0700

    Update gpiosave.php

--- srv/http/gpiosave.php
+++ srv/http/gpiosave.php
@@ -1,50 +1,7 @@
 <?php
-$redis = new Redis(); 
-$redis->pconnect( '127.0.0.1' );
-
-if ( isset( $_GET[ 'udac' ] ) ) {
-	include( '/srv/http/app/libs/runeaudio.php' );
-	
-	wrk_audioOutput($redis, 'refresh'); // refresh acards list: cat /proc/asound/cards
-	
-	// fix hw:0,N - missing N after wrk_audioOutput($redis, 'refresh')
-	$acards = $redis->hGetAll( 'acards' );
-	foreach ( $acards as $key => $value ) {
-		preg_match( '/"id":"."/', $value, $match );
-		$id = preg_replace( '/"id":"(.)"/', '${1}', $match[ 0 ] );
-		$subdevice = $id ? $id - 1 : 0;
-		$value1 = preg_replace( '/(hw:.,)/', '${1}'.$subdevice, $value );
-		$redis->hSet( 'acards', $key, $value1 );
-	}
-	
-	$ao = $redis->get( 'ao' );
-	if ( !$acards[ $ao ] ) {
-		$redis->set( 'ao', 'bcm2835 ALSA_1' );
-		wrk_mpdconf( $redis, 'switchao', 'bcm2835 ALSA_1' );
-		wrk_mpdconf( $redis, 'restart' );
-	}
-	
-	die();
-}
-
-$redis->set( 'enablegpio', $_POST[ 'enable' ] );
-
-if ( isset( $_GET[ 'ao' ] ) ) {
-	$aogpio = $redis->get( 'ao' );
-	$volume = $redis->get( 'volume' );
-	$mpdconf = $redis->hGetAll( 'mpdconf' );
-	
-	$redis->set( 'aogpio', $aogpio );
-	$redis->set( 'volumegpio', $volume );
-	foreach ( $mpdconf as $key => $value ) {
-		$redis->hSet( 'mpdconfgpio', $key, $value );
-	}
-	
-	die();
-}
-
 $gpio = array(
-	  'pin'    => array(
+	  'enable'  => array( 'enable'  => $_POST[ 'enable' ] )
+	, 'pin'    => array(
 		  'pin1'   => $_POST[ 'pin1' ]
 		, 'pin2'   => $_POST[ 'pin2' ]
 		, 'pin3'   => $_POST[ 'pin3' ]
