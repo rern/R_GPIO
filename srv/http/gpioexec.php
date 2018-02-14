99e8ce32ade3113cf7c7f0f52b67dfd8808a028b
rern <rernrern@gmail.com>
2018-02-14 12:55:28 +0700

    Update gpioexec.php

--- srv/http/gpioexec.php
+++ srv/http/gpioexec.php
@@ -2,9 +2,9 @@
 $onoffpy = $_GET[ 'onoffpy' ];
 
 if ( $onoffpy === 'gpiotimer.py' ) {
-	exec( '/usr/bin/sudo /usr/bin/killall -9 /root/gpiotimer.py &> /dev/null' );
-	exec( '/usr/bin/sudo /root/gpiotimer.py &> /dev/null &' );
+	exec( '/usr/bin/killall -9 /root/gpiotimer.py &> /dev/null' );
+	exec( '/root/gpiotimer.py &> /dev/null &' );
 	die();
 }
 
-echo exec( '/usr/bin/sudo /root/'.$onoffpy );
+echo exec( '/root/'.$onoffpy );
