14db63782297ab7d9bef1a08a32d027c64c63269
rern <rernrern@gmail.com>
2018-03-20 22:07:09 +0700

    Add files via upload

--- srv/http/gpioexec.php
+++ srv/http/gpioexec.php
@@ -4,6 +4,9 @@ $onoffpy = $_GET[ 'onoffpy' ];
 if ( $onoffpy === 'gpiotimer.py' ) {
 	exec( '/usr/bin/sudo /usr/bin/killall -9 /root/gpiotimer.py &> /dev/null' );
 	exec( '/usr/bin/sudo /root/gpiotimer.py &> /dev/null &' );
+	// broadcast to remove pnotify
+	exec( '/usr/bin/curl -s -v -X POST "http://localhost/pub?id=gpio" -d "{ \"state\": \"RESET\" }"' );
+	
 	die();
 }
 
