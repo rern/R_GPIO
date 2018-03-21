8cc098ce4eab07b392a04682d5c7073518003b54
rern <rernrern@gmail.com>
2018-03-21 08:26:02 +0700

    Update gpioexec.php

--- srv/http/gpioexec.php
+++ srv/http/gpioexec.php
@@ -2,7 +2,7 @@
 $onoffpy = $_GET[ 'onoffpy' ];
 
 if ( $onoffpy === 'gpiotimer.py' ) {
-	exec( '/usr/bin/sudo /usr/bin/killall -9 /root/gpiotimer.py &> /dev/null' );
+	exec( '/usr/bin/sudo /usr/bin/killall -9 gpiotimer.py &> /dev/null' );
 	exec( '/usr/bin/sudo /root/gpiotimer.py &> /dev/null &' );
 	// broadcast to remove idle reset infobox
 	exec( '/usr/bin/curl -s -v -X POST "http://localhost/pub?id=gpio" -d "{ \"state\": \"RESET\" }"' );
