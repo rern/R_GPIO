4ec622bd5aa58ea7e15bf924794b2e69c925acf8
rern <rernrern@gmail.com>
2018-07-14 12:22:51 +0700

    Add files via upload

--- srv/http/gpioexec.php
+++ srv/http/gpioexec.php
@@ -1,13 +1,23 @@
 <?php
-$onoffpy = $_GET[ 'onoffpy' ];
+$command = $_GET[ 'command' ];
 
-if ( $onoffpy === 'gpiotimer.py' ) {
+if ( $command === 'timer' ) {
 	exec( '/usr/bin/sudo /usr/bin/killall -9 gpiotimer.py &> /dev/null' );
 	exec( '/usr/bin/sudo /root/gpiotimer.py &> /dev/null &' );
 	// broadcast to remove idle reset infobox
 	exec( '/usr/bin/curl -s -v -X POST "http://localhost/pub?id=gpio" -d "{ \"state\": \"RESET\" }"' );
-	
-	die();
+} else if ( $command === 'reboot' ) {
+	$part = exec( '/usr/bin/sudo /usr/bin/mount | grep "on / " | cut -d" " -f1' );
+	$bootpart = substr( $part, -1 ) - 1;
+	$kernel = exec( '/usr/bin/sudo /usr/bin/uname -r | cut -d"-" -f1' );
+	if ( $kernel >= '4.4.39' ) {
+		exec( "/var/www/command/rune_shutdown; /usr/bin/reboot $bootpart" );
+	} else {
+		exec( "/bin/echo $bootpart > /sys/module/bcm2709/parameters/reboot_part" );
+		exec( "/var/www/command/rune_shutdown; /usr/bin/reboot" );
+	}
+} else if ( $command === 'poweroff' ) {
+	exec( "/var/www/command/rune_shutdown;" );
+} else {
+	echo exec( '/usr/bin/sudo /root/'.$command );
 }
-
-echo exec( '/usr/bin/sudo /root/'.$onoffpy );
