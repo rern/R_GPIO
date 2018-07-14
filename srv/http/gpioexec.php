1ef04844d6ad1a064640f4a7b611e1b689586111
rern <rernrern@gmail.com>
2018-07-14 13:04:54 +0700

    Add files via upload

--- srv/http/gpioexec.php
+++ srv/http/gpioexec.php
@@ -10,15 +10,11 @@ if ( $command === 'timer' ) {
 	exec( '/usr/bin/sudo /root/gpiooff.py' );
 	$part = exec( '/usr/bin/sudo /usr/bin/mount | grep "on / " | cut -d" " -f1' );
 	$bootpart = substr( $part, -1 ) - 1;
-	$kernel = exec( '/usr/bin/sudo /usr/bin/uname -r | cut -d"-" -f1' );
-	if ( $kernel >= '4.4.39' ) {
-		exec( "/var/www/command/rune_shutdown; /usr/bin/reboot $bootpart" );
-	} else {
-		exec( "/bin/echo $bootpart > /sys/module/bcm2709/parameters/reboot_part" );
-		exec( "/var/www/command/rune_shutdown; /usr/bin/reboot" );
-	}
+	// for older kernel
+	exec( "/bin/echo $bootpart > /sys/module/bcm2709/parameters/reboot_part" );
+	exec( "/var/www/command/rune_shutdown; /usr/bin/reboot $bootpart" );
 } else if ( $command === 'poweroff' ) {
-	exec( "/var/www/command/rune_shutdown;" );
+	exec( '/usr/bin/sudo /root/gpiooff.py; /var/www/command/rune_shutdown;' );
 } else {
 	echo exec( '/usr/bin/sudo /root/'.$command );
 }
