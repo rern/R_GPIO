ae21fb7de05525b0fa97308e4ed8c4c963b637a1
rern <rernrern@gmail.com>
2018-08-06 20:26:28 +0700

    Update uninstall_gpio.sh

--- usr/local/bin/uninstall_gpio.sh
+++ usr/local/bin/uninstall_gpio.sh
@@ -22,11 +22,11 @@ rm -v /srv/http/assets/js/vendor/bootstrap-select-1.12.1.min.js
 # restore modified files #######################################
 echo -e "$bar Restore modified files ..."
 
+[[ -e /usr/local/bin/uninstall_enha.sh ]] && backup=.backup
+
 files="
-/srv/http/app/templates/header.php
-/srv/http/app/templates/footer.php
-/srv/http/app/templates/header.php.backup
-/srv/http/app/templates/footer.php.backup
+/srv/http/app/templates/header.php$backup
+/srv/http/app/templates/footer.php$backup
 /root/.xbindkeysrc
 "
 restorefile $files
