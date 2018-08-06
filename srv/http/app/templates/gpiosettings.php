6a080de115e90eaa99e9030c612b3fda7732787e
rern <rernrern@gmail.com>
2018-08-06 18:16:40 +0700

    Update gpiosettings.php

--- srv/http/app/templates/gpiosettings.php
+++ srv/http/app/templates/gpiosettings.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>GPIO</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="msapplication-tap-highlight" content="no" />
-    <link rel="stylesheet" href="assets/css/runeui.css">
-    <link rel="stylesheet" href="assets/css/addonsinfo.css">
-    <link rel="stylesheet" href="assets/css/gpiosettings.css">
-    <link rel="shortcut icon" href="assets/img/favicon.ico">
-</head>
-
 <?php
 $file = '/srv/http/gpio.json';
 $fileopen = fopen( $file, 'r' );
@@ -198,13 +182,3 @@ function opttime( $n, $minimum ) {
 
 </form>
 </div>
-
-<script src="assets/js/vendor/jquery-2.1.0.min.js"></script>
-<script src="assets/js/vendor/bootstrap.min.js"></script>
-<script src="assets/js/vendor/bootstrap-select-1.12.1.min.js"></script>
-<script src="assets/js/vendor/pnotify.custom.min.js"></script>
-<script src="assets/js/addonsinfo.js"></script>
-<script src="assets/js/gpiosettings.js"></script>
-
-</body>
-</html>
