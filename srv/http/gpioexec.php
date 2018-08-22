7bcaf5d48e65ef52685b1dab44a1aea44c29aab0
rern <rernrern@gmail.com>
2018-08-22 22:14:14 +0700

    Add files via upload

--- srv/http/gpioexec.php
+++ srv/http/gpioexec.php
@@ -11,8 +11,12 @@ if ( $command === 'timer' ) {
 	$part = exec( '/usr/bin/sudo /usr/bin/mount | grep "on / " | cut -d" " -f1' );
 	$bootpart = substr( $part, -1 ) - 1;
 	// for older kernel
-	exec( "/usr/bin/sudo /bin/echo $bootpart > /sys/module/bcm2709/parameters/reboot_part" );
-	exec( "/usr/bin/sudo /var/www/command/rune_shutdown; /usr/bin/reboot $bootpart" );
+	$buildversion = exec( 'redis-cli get buildversion' );
+	if ( $buildversion === 'beta-20160313' ) {
+		exec( "/usr/bin/sudo /bin/echo $bootpart > /sys/module/bcm2709/parameters/reboot_part" );
+	} else {
+		exec( "/usr/bin/sudo /var/www/command/rune_shutdown; /usr/bin/reboot $bootpart" );
+	}
 } else if ( $command === 'poweroff' ) {
 	exec( '/usr/bin/sudo /root/gpiooff.py; /var/www/command/rune_shutdown;' );
 } else {
