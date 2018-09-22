1efd8407c5e4e9f6154fa228a66b246783002bad
rern <rernrern@gmail.com>
2018-09-22 11:00:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,6 +40,7 @@ echo $file
 [[ -e $file.backup ]] && file=$file.backup
 
 string=$( cat <<'EOF'
+<input id="bootstrapmincss" type="hidden" value="<?=$this->asset('/css/bootstrap.min.css')?>">
 <input id="bootstrapselectmincss" type="hidden" value="<?=$this->asset('/css/bootstrap-select.min.css')?>">
 <input id="gpiosettingscss" type="hidden" value="<?=$this->asset('/css/gpiosettings.css')?>">
 <input id="gpiosettingsjs" type="hidden" value="<?=$this->asset('/js/gpiosettings.js')?>">
