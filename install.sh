d4e4c954d33c6fffcd579941a6c1fcc729389f17
rern <rernrern@gmail.com>
2019-01-29 22:17:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,16 +39,6 @@ echo $file
 
 [[ -e $file.backup ]] && file=$file.backup
 
-string=$( cat <<'EOF'
-<input id="bootstrapmincss" type="hidden" value="<?=$this->asset('/css/bootstrap.min.css')?>">
-<input id="bootstrapselectmincss" type="hidden" value="<?=$this->asset('/css/bootstrap-select.min.css')?>">
-<input id="gpiosettingscss" type="hidden" value="<?=$this->asset('/css/gpiosettings.css')?>">
-<input id="gpiosettingsjs" type="hidden" value="<?=$this->asset('/js/gpiosettings.js')?>">
-<input id="gpiopin" type="hidden" value="<?=$this->asset('/img/RPi3_GPIO.svg')?>">
-EOF
-)
-insertH 'jquery-2.1.0.min.js'
-
 appendAsset 'fastclick.min.js' 'gpio.js'
 #----------------------------------------------------------------------------------
 # Dual boot
