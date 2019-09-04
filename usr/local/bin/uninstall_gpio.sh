7be0b8387f009db023c8e4cad67286f27a7b41ca
rern <rernrern@gmail.com>
2019-09-04 08:12:18 +0700

    Update uninstall_gpio.sh

--- usr/local/bin/uninstall_gpio.sh
+++ usr/local/bin/uninstall_gpio.sh
@@ -14,7 +14,6 @@ echo -e "$bar Remove files ..."
 rm -v /root/gpio*
 rm -v /srv/http/gpio*
 rm -v /srv/http/assets/css/gpio*
-rm -rv /srv/http/assets/img/gpio
 rm -v /srv/http/assets/img/RPi3_GPIO*
 rm -v /srv/http/assets/js/gpio*
 
