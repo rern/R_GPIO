961096371077c58190a1104ad33fa9f4c29107d5
rern <rernrern@gmail.com>
2019-09-04 14:25:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ EOF
 fi
 
 # set permission #######################################
+chmod 755 /root/gpio*
 usermod -a -G root http # add user http to group root to allow /dev/gpiomem access
 #chmod g+rw /dev/gpiomem # allow group to access set in gpio.py set for every boot
 
