1b31b5da202c55c34a4c05cd4ebf67e4b0e7ab5a
rern <rernrern@gmail.com>
2019-09-05 11:30:21 +0700

    Update uninstall_gpio.sh

--- usr/local/bin/uninstall_gpio.sh
+++ usr/local/bin/uninstall_gpio.sh
@@ -15,6 +15,7 @@ rm -v /root/gpio*
 rm -v /srv/http/gpio*
 rm -v /srv/http/assets/css/gpio*
 rm -v /srv/http/assets/img/RPi3_GPIO*
+rm -rv /srv/http/assets/img/gpio
 rm -v /srv/http/assets/js/gpio*
 
 echo -e "$bar Restore files ..."
